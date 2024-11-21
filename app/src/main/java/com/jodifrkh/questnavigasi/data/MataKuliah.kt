61554babcc4572a1813bdb79a9b82ade09198e2c
KhodamIblis <jodinurfarkhani04@gmail.com>
2024-11-21 16:44:43 +0700

    membuat variabel options yang berisikan list dari nama - nama MataKuliah

--- app/src/main/java/com/jodifrkh/questnavigasi/data/MataKuliah.kt
+++ app/src/main/java/com/jodifrkh/questnavigasi/data/MataKuliah.kt
@@ -1,4 +1,12 @@
 package com.jodifrkh.questnavigasi.data
 
 object MataKuliah {
+    val options = listOf(
+        "Agama Islam dan Kemuhammadiyahan",
+        "Pengembangan Aplikasi Web",
+        "Pancasila",
+        "Dasar Teknologi Informasi",
+        "Fisika dan Elektronika",
+        "Logika Teknik Pemrogramman"
+    )
 }
