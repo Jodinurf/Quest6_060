939e89bcc01269798f1a781f8b82114364d600c2
KhodamIblis <jodinurfarkhani04@gmail.com>
2024-11-21 17:54:50 +0700

    membuat fungsi RencanaStudyView dengan 3 parameter yaitu object mahasiswa dari kelas Mahasiswa, onSubmitButtonClicked untuk mengirim list data, dan onBackButtonClicked untuk kembali ke halaman sebelumnya

--- app/src/main/java/com/jodifrkh/questnavigasi/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/jodifrkh/questnavigasi/ui/view/screen/RencanaStudyView.kt
@@ -1,2 +1,11 @@
 package com.jodifrkh.questnavigasi.ui.view.screen
 
+import com.jodifrkh.questnavigasi.model.Mahasiswa
+
+fun RencanaStudyView(
+    mahasiswa: Mahasiswa,
+    onSubmitButtonClicked: (MutableList<String>) -> Unit,
+    onBackButtonClicked: () -> Unit
+){
+
+}
