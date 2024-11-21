0782302161c1931c3cf5b0a6751e43990ff575aa
KhodamIblis <jodinurfarkhani04@gmail.com>
2024-11-21 23:06:59 +0700

    membuat fungsi MahasiswaApp yang berisikan object - object yang diambil class MahasiswaViewModel, RencanaStudyViewModel, dan NavHostController

--- app/src/main/java/com/jodifrkh/questnavigasi/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/jodifrkh/questnavigasi/navigation/PengelolaHalaman.kt
@@ -1,8 +1,33 @@
 package com.jodifrkh.questnavigasi.navigation
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.jodifrkh.questnavigasi.ui.view.screen.MahasiswaFormView
+import com.jodifrkh.questnavigasi.ui.view.screen.SplashView
+import com.jodifrkh.questnavigasi.ui.view.viewModel.MahasiswaViewModel
+import com.jodifrkh.questnavigasi.ui.view.viewModel.RencanaStudyViewModel
+
+
 enum class Halaman{
     Splash,
     Mahasiswa,
     MataKuliah,
     Tampil
+}
+
+@Composable
+fun MahasiswaApp(
+    modifier: Modifier = Modifier,
+    mahasiswaViewModel: MahasiswaViewModel = viewModel(),
+    krsViewModel: RencanaStudyViewModel = viewModel(),
+    navController: NavHostController = rememberNavController()
+) {
+
 }
