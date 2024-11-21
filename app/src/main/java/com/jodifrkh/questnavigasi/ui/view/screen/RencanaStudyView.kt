b90e23fb47a024fb902849332000674f651952a9
KhodamIblis <jodinurfarkhani04@gmail.com>
2024-11-21 18:01:15 +0700

    membuat variable checked untuk pilihan kelas, chosenDropDown untuk daftar matakuliah, listData untuk menyimpan hasil pilihan dari dropdown dan pilihanKelas

--- app/src/main/java/com/jodifrkh/questnavigasi/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/jodifrkh/questnavigasi/ui/view/screen/RencanaStudyView.kt
@@ -1,11 +1,23 @@
 package com.jodifrkh.questnavigasi.ui.view.screen
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import com.jodifrkh.questnavigasi.model.Mahasiswa
 
+@Composable
 fun RencanaStudyView(
     mahasiswa: Mahasiswa,
     onSubmitButtonClicked: (MutableList<String>) -> Unit,
     onBackButtonClicked: () -> Unit
 ){
+    var chosenDropdown by remember {
+        mutableStateOf("")
+    }
+    var checked by remember { mutableStateOf(false) }
+    var pilihanKelas by remember { mutableStateOf("") }
+    var listData: MutableList<String> = mutableListOf(chosenDropdown, pilihanKelas)
 
 }
