0c77cd9a110e1b6af9180adb488cbaeeae6e3faf
KhodamIblis <jodinurfarkhani04@gmail.com>
2024-11-21 23:03:39 +0700

    membuat RencanaStudyViewModel untuk menyimpan data - data inputan di halaman RencanaStudyView

--- app/src/main/java/com/jodifrkh/questnavigasi/ui/view/viewModel/RencanaStudyViewModel.kt
+++ app/src/main/java/com/jodifrkh/questnavigasi/ui/view/viewModel/RencanaStudyViewModel.kt
@@ -1,4 +1,28 @@
 package com.jodifrkh.questnavigasi.ui.view.viewModel
 
-class RencanaStudyViewModel {
+import androidx.lifecycle.ViewModel
+import com.jodifrkh.questnavigasi.model.RencanaStudi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class RencanaStudyViewModel:ViewModel() {
+    private val _krsState = MutableStateFlow(RencanaStudi())
+    val krsStateUi: StateFlow<RencanaStudi> = _krsState.asStateFlow()
+
+    fun setMataKuliah(mkPilihan: String) {
+        _krsState.update { stateMK -> stateMK.copy(mataKuliah = mkPilihan) }
+    }
+
+    fun setKelas(kelasPilihan: String){
+        _krsState.update { stateKelas -> stateKelas.copy(kelas = kelasPilihan) }
+    }
+
+    fun saveDataKRS(ls: MutableList<String>){
+        _krsState.update { status -> status.copy(
+            mataKuliah = ls[0],
+            kelas = ls [1]
+        ) }
+    }
 }
