fa661d17276eed128173a1bbeefcaa5ea8958ac5
KhodamIblis <jodinurfarkhani04@gmail.com>
2024-11-21 17:10:23 +0700

    membuat fungsi SplashView sebagai isi dari halaman pada SplashView dengan parameter onMulaiButton yang nantinya akan menjadi button navigasi ke halaman selanjutnya

--- app/src/main/java/com/jodifrkh/questnavigasi/ui/view/screen/SplashView.kt
+++ app/src/main/java/com/jodifrkh/questnavigasi/ui/view/screen/SplashView.kt
@@ -1,2 +1,26 @@
 package com.jodifrkh.questnavigasi.ui.view.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.jodifrkh.questnavigasi.R
+
+@Composable
+fun SplashView(
+    onMulaiButton: () -> Unit
+){
+
+}
